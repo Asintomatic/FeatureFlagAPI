7e6e838270a40095774ed902fc33cec8b58188da
Alvaro <ascintomatic@gmail.com>
2025-09-06 01:11:57 +0200

    se crean excepciones personalizadas para la gestion de errores de seguridad

---
+++ src/main/java/com/bytescolab/featureflag/exception/TokenInvalidoException.java
@@ -0,0 +1,7 @@
+package com.bytescolab.featureflag.exception;
+
+public class TokenInvalidoException extends RuntimeException {
+    public TokenInvalidoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/bytescolab/featureflag/exception/TokenMalFormadoException.java
@@ -0,0 +1,7 @@
+package com.bytescolab.featureflag.exception;
+
+public class TokenMalFormadoException extends RuntimeException {
+    public TokenMalFormadoException(String message) {
+        super(message);
+    }
+}
