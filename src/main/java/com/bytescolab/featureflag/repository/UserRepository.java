c7a947821a5958def2c80a904a35ae5af2aa2382
Alvaro <ascintomatic@gmail.com>
2025-09-06 01:14:28 +0200

    se crea configuracion de Swagger y metodos de user en service, repository para gestion de metodos en el apartado security

--- src/main/java/com/bytescolab/featureflag/repository/UserRepository.java
+++ src/main/java/com/bytescolab/featureflag/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.bytescolab.featureflag.repository;
 
-public interface UserRepository {
+import com.bytescolab.featureflag.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername (String name);
+    boolean existsByUsername(String username);
 }
---
+++ src/main/java/com/bytescolab/featureflag/service/user/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.bytescolab.featureflag.service.user;
+
+import com.bytescolab.featureflag.model.entity.User;
+import com.bytescolab.featureflag.repository.UserRepository;
+import com.bytescolab.featureflag.security.auth.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * Carga un usuario desde la base de datos por su username.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return new CustomUserDetails(user);
+    }
+
+
+}
