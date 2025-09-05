be5db92811eff7ff62ad29f2ffd1e4ee22d8448d
Alvaro <ascintomatic@gmail.com>
2025-09-06 01:11:21 +0200

    Creada configuración de seguridad y jwt utils y filter

--- src/main/java/com/bytescolab/featureflag/security/jwt/JwtFilter.java
+++ src/main/java/com/bytescolab/featureflag/security/jwt/JwtFilter.java
@@ -1,4 +1,86 @@
 package com.bytescolab.featureflag.security.jwt;
 
-public class JwtFilter {
+import com.bytescolab.featureflag.service.user.UserDetailsService;
+import com.bytescolab.featureflag.exception.TokenExpiradoException;
+import com.bytescolab.featureflag.exception.TokenInvalidoException;
+import com.bytescolab.featureflag.exception.TokenMalFormadoException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        //Se recoge el token de las cabeceras
+        final String authHeader = request.getHeader("Authorization");
+        log.info("Recogemos el token {}", authHeader );
+
+        //Si no tenemos header o no corresponde con Bearer, no se continua.
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            log.warn("Token nulo o  formato inválido.");
+            return;
+        }
+
+        String jwt = authHeader.substring(7);
+
+        try {
+            String username = jwtUtils.extractUsername(jwt);
+
+            //Comprobamos que el username está dentro del token, si no está autenticado
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                //recogemos el usuario de bbdd
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                //si token válido, metemos en contexto
+                if (jwtUtils.isTokenValid(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+
+                    log.info("Usuario autenticado correctamente: {}", username);
+                    log.debug("Rol del usuario: {}", userDetails.getAuthorities());
+
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                } else {
+                    log.error("Token no válido para usuario {}", username);
+                }
+            }
+
+        } catch (TokenExpiradoException e) {
+            log.error("Token expirado: {}", e.getMessage());
+            throw new TokenExpiradoException("El token ha expirado");
+        } catch (TokenMalFormadoException e) {
+            log.error("Token mal formado: {}", e.getMessage());
+            throw new TokenMalFormadoException("El token está mal formado");
+        } catch (TokenInvalidoException e) {
+            log.error("Error al validar el token JWT: {}", e.getMessage());
+            throw new TokenInvalidoException("El token no ha podido ser validado.");
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/com/bytescolab/featureflag/security/jwt/JwtUtils.java
+++ src/main/java/com/bytescolab/featureflag/security/jwt/JwtUtils.java
@@ -1,4 +1,61 @@
 package com.bytescolab.featureflag.security.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
 public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expirationMs}")
+    private long jwtExpirationMs;
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim("roles", userDetails.getAuthorities()) // 👈 metemos los roles
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return expiration.before(new Date());
+    }
+
+    private SecretKey getSigningKey(){
+        byte[] keyBytes = Base64.getDecoder().decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
