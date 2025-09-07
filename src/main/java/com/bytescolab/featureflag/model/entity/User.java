792e8261650955899252f05cbdea7fa8630702f1
jsbrb <javibrb87@gmail.com>
2025-09-07 09:53:44 +0200

    Añadidas anotaciones a entidad 'user'

--- src/main/java/com/bytescolab/featureflag/model/entity/User.java
+++ src/main/java/com/bytescolab/featureflag/model/entity/User.java
@@ -1,14 +1,30 @@
 package com.bytescolab.featureflag.model.entity;
 
 import com.bytescolab.featureflag.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
+
     private String username;
+
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private Role rol;
 
 }
