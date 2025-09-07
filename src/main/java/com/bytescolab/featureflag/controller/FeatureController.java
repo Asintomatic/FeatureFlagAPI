f975c022dc24f51f9965eb8583a23c7c550b0d96
jsbrb <javibrb87@gmail.com>
2025-09-07 12:17:14 +0200

    Realizado controller de feature

--- src/main/java/com/bytescolab/featureflag/controller/FeatureController.java
+++ src/main/java/com/bytescolab/featureflag/controller/FeatureController.java
@@ -1,33 +1,37 @@
 package com.bytescolab.featureflag.controller;
 
+import com.bytescolab.featureflag.dto.feature.FeatureDTO;
+import com.bytescolab.featureflag.dto.feature.FeatureDetailDTO;
+import com.bytescolab.featureflag.service.feature.FeatureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/feature")
 public class FeatureController {
 
-    //Crear nueva feature
-    //@PostMapping createFeature ("/api/features")
-    //FIN Crear nueva feature
+    @Autowired
+    private FeatureService featureService;
 
-    //Listar features
-    //@GetMapping findAllFeatures ("/api/features")
-    //FIN Listar features
+    //Listar todas las features
+    @GetMapping
+    public ResponseEntity<List<FeatureDTO>> getAllFeatures(){
+        List<FeatureDTO> features = featureService.getAllFeatures();
+        return ResponseEntity.ok(features);
+    }
 
-    //Detalle de feature
-    //@GetMapping findDetailFeature ("/api/features/{id}")
-    //FIN Detalle de feature
-
-    //Activar feature para cliente/entorno
-    //@PostMapping activeFeature ("/api/features/{id}/enable")
-    //FIN Activar feature para cliente/entorno
-
-    //Desactivar feature para cliente/entorno
-    //@PostMapping disableFeature ("/api/features/{id}/disable")
-    //FIN Desactivar feature para cliente/entorno
-
-    //Verificar si una feature está activa
-    //@GetMapping verifyFeature ("/api/features/check")
+    //Listar feature por id
+    @GetMapping("/{id}")
+    public ResponseEntity<FeatureDetailDTO> getFeatureById(@PathVariable UUID id){
+        FeatureDetailDTO feature = featureService.getFeatureById(id);
+        return ResponseEntity.ok(feature);
+    }
 
 }
